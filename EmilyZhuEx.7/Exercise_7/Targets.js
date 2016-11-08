5d053efdd51ccaff8eb810814c56a87f1001a68c
Ezhu156 <ez682@nyu.edu>
2016-11-08 13:00:09 -0500

    Update Targets.js

--- EmilyZhuEx.7/Exercise_7/Targets.js
+++ EmilyZhuEx.7/Exercise_7/Targets.js
@@ -1,60 +1,106 @@
 function Targets() {
-  fill(0, 50);
-  //rect(137, 2, 75, 75);
+  if (value === 0) {
+    //target 1
+    image(target, 880 - (target.width / 2), 205 - (target.height / 2));
+    //target 2
+    image(target, 650 - (target.width / 2), 350 - (target.height / 2));
+    text("2", 635, 310);
+    //target 3
+    image(target, 175 - (target.width / 2), 350 - (target.height / 2));
+    text("3", 160, 310);
+    //target 4
+    image(target, 880 - (target.width / 2), 485 - (target.height / 2));
+    text("4", 920, 500);
+    //target 5
+    image(target, 50 - (target.width / 2), 585 - (target.height / 2));
+    text("5", 90, 583);
+    //target6
+    image(target, 468 - (target.width / 2), 720 - (target.height / 2));
+    text("6", 455, 685);
 
-  //target 1
-  if (((mouseX >= 842) && (mouseX <= 917)) && ((mouseY >= 167) && (mouseY <= 243))) {
-    if (mouseIsPressed) {
-      image(alien, 880 - (alien.width / 4), 205 - (alien.height / 4), 100, 100);
-    } //end if
-  } //end mousePressed
-  else image(target, 880 - (target.width / 2), 205 - (target.height / 2));
-
-
-  //target 2
-  if (((mouseX >= 612) && (mouseX <= 687)) && ((mouseY >= 312) && (mouseY <= 487))) {
-    if (mouseIsPressed) {
-      image(alien, 640 - (alien.width / 4), 340 - (alien.height / 4), 100, 100);
-    } //end if
-  } //end mousePressed
-  else image(target, 650 - (target.width / 2), 350 - (target.height / 2));
-  // text("2", 635, 310);
-
-  //target 3
-  if (((mouseX >= 137) && (mouseX <= 212)) && ((mouseY >= 312) && (mouseY <= 487))) {
-    if (mouseIsPressed) {
-      image(alien, 170 - (alien.width / 4), 330 - (alien.height / 4), 100, 100);
-    } //end if
-  } //end mousePressed
-  else image(target, 175 - (target.width / 2), 350 - (target.height / 2));
-  //text("3", 160, 310);
-
-  //target 4
-  if (((mouseX >= 842) && (mouseX <= 917)) && ((mouseY >= 447) && (mouseY <= 522))) {
-    if (mouseIsPressed) {
-      image(alien, 890 - (alien.width / 4), 470 - (alien.height / 4), 100, 100);
-    } //end if
-  } //end mousePressed
-  else image(target, 880 - (target.width / 2), 485 - (target.height / 2));
-  // text("4", 920, 500);
-
-  //target 5
-  if (((mouseX >= 12) && (mouseX <= 87)) && ((mouseY >= 547) && (mouseY <= 622))) {
-    if (mouseIsPressed) {
-      image(alien, 50 - (alien.width / 4), 570 - (alien.height / 4), 100, 100);
-    } //end if
-  } //end mousePressed
-  else image(target, 50 - (target.width / 2), 585 - (target.height / 2));
-  // text("5", 90, 583);
-
-  //target6
-  if (((mouseX >= 430) && (mouseX <= 505)) && ((mouseY >= 682) && (mouseY <= 757))) {
-    if (mouseIsPressed) {
-      image(alien, 460 - (alien.width / 4), 700 - (alien.height / 4), 100, 100);
-    } //end if
-  } //end mousePressed
-  else image(target, 468 - (target.width / 2), 720 - (target.height / 2));
-  // text("6", 455, 685);
-
-
-} //end targets
+  } //end 0
+  if (value == 1) {
+    //target 1
+    image(alien, 880 - (alien.width / 4), 205 - (alien.height / 4), 100, 100);
+    //target 2
+    image(target, 650 - (target.width / 2), 350 - (target.height / 2));
+    //target 3
+    image(target, 175 - (target.width / 2), 350 - (target.height / 2));
+    //target 4
+    image(target, 880 - (target.width / 2), 485 - (target.height / 2));
+    //target 5
+    image(target, 50 - (target.width / 2), 585 - (target.height / 2));
+    //target6
+    image(target, 468 - (target.width / 2), 720 - (target.height / 2));
+  } //end 1
+  if (value == 2) {
+    //target 2
+    image(alien, 640 - (alien.width / 4), 340 - (alien.height / 4), 100, 100);
+    //target 1
+    image(target, 880 - (target.width / 2), 205 - (target.height / 2));
+    //target 3
+    image(target, 175 - (target.width / 2), 350 - (target.height / 2));
+    //target 4
+    image(target, 880 - (target.width / 2), 485 - (target.height / 2));
+    //target 5
+    image(target, 50 - (target.width / 2), 585 - (target.height / 2));
+    //target6
+    image(target, 468 - (target.width / 2), 720 - (target.height / 2));
+  } //end 2
+  if (value == 3) {
+    //target 3
+    image(alien, 170 - (alien.width / 4), 330 - (alien.height / 4), 100, 100);
+    //target 1
+    image(target, 880 - (target.width / 2), 205 - (target.height / 2));
+    //target 2
+    image(target, 650 - (target.width / 2), 350 - (target.height / 2));
+    //target 4
+    image(target, 880 - (target.width / 2), 485 - (target.height / 2));
+    //target 5
+    image(target, 50 - (target.width / 2), 585 - (target.height / 2));
+    //target6
+    image(target, 468 - (target.width / 2), 720 - (target.height / 2));
+  } //end 3
+  if (value == 4) {
+    //target 4
+    image(alien, 890 - (alien.width / 4), 470 - (alien.height / 4), 100, 100);
+    //target 1
+    image(target, 880 - (target.width / 2), 205 - (target.height / 2));
+    //target 2
+    image(target, 650 - (target.width / 2), 350 - (target.height / 2));
+    //target 3
+    image(target, 175 - (target.width / 2), 350 - (target.height / 2));
+    //target 5
+    image(target, 50 - (target.width / 2), 585 - (target.height / 2));
+    //target6
+    image(target, 468 - (target.width / 2), 720 - (target.height / 2));
+  } //end 4
+  if (value == 5) {
+    //target 5
+    image(alien, 50 - (alien.width / 4), 570 - (alien.height / 4), 100, 100);
+    //target 1
+    image(target, 880 - (target.width / 2), 205 - (target.height / 2));
+    //target 2
+    image(target, 650 - (target.width / 2), 350 - (target.height / 2));
+    //target 3
+    image(target, 175 - (target.width / 2), 350 - (target.height / 2));
+    //target 4
+    image(target, 880 - (target.width / 2), 485 - (target.height / 2));
+    //target6
+    image(target, 468 - (target.width / 2), 720 - (target.height / 2));
+  } //end 5
+  if (value == 6) {
+    //target6
+    image(alien, 460 - (alien.width / 4), 700 - (alien.height / 4), 100, 100);
+    //target 1
+    image(target, 880 - (target.width / 2), 205 - (target.height / 2));
+    //target 2
+    image(target, 650 - (target.width / 2), 350 - (target.height / 2));
+    //target 3
+    image(target, 175 - (target.width / 2), 350 - (target.height / 2));
+    //target 4
+    image(target, 880 - (target.width / 2), 485 - (target.height / 2));
+    //target 5
+    image(target, 50 - (target.width / 2), 585 - (target.height / 2));
+  } //end 6
+} //end target
