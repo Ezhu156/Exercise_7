cd26717df405820b3f9445b7bfd140b15f19f611
Ezhu156 <ez682@nyu.edu>
2016-12-13 15:26:54 -0500

    Update Bullet.js

--- EmilyZhuEx.7/Exercise_7/Bullet.js
+++ EmilyZhuEx.7/Exercise_7/Bullet.js
@@ -2,17 +2,18 @@ function Bullet(xloc, yloc) {
   var x = xloc;
   var y = yloc;
   
-  this.display = function(r, g, b) { 
-  if (mouseIsPressed){
+  this.display = function() { 
+  if (mouseIsPressed){ //if the mouse is pressed the the bullet will be where the mouse is
+                      //it will also be in the middle of the ner gun scope
       x = mouseX;
       y = mouseY;
   }else{
     x=mouseX+50;
     y=890;
-  }
-      // Body
+  } //end else
+      // the circle that represents the bullet being fired
       noStroke();
-      fill(r, g, b);
+      fill(0,100);
       ellipse(x, y, 25, 25);
     } //end display
 } // end Bullet
