c3a7e453a11ded486a3e0f8f312863eeeeeaa056
Ezhu156 <ez682@nyu.edu>
2016-12-13 16:02:49 -0500

    Update Boxes.js

--- EmilyZhuEx.7/Exercise_7/Boxes.js
+++ EmilyZhuEx.7/Exercise_7/Boxes.js
@@ -3,15 +3,15 @@ function Boxes() {
   var velocity
   
   this.show = function() {
-    //box 1
-    image(box, 375 - (box.width / 2), 1000 - (box.height / 2));
+    //square 1
+    image(square, 375 - (square.width / 2), 1000 - (square.height / 2));
     if (((x > 100) && (x <= 150)) && (y >= 875)) {
       image(alien, 0, 0, c, d);
       if ((c <= 1000) && (d <= 1000)) c += 1.5, d += 1.5;
     } //end if
 
-    //box 2
-    image(box, 575 - (box.width / 2), 1000 - (box.height / 2));
+    //square 2
+    image(square, 575 - (square.width / 2), 1000 - (square.height / 2));
     tint(mouseX, 0, mouseY);
     if (((x > 300) && (x <= 350)) && (y >= 875)) {
       for (var e = -25; e <= width - 25; e += 250) {
@@ -26,23 +26,23 @@ function Boxes() {
       } //end for f
     } //end if
     noTint();
-    //box 3
-    image(box, 900 - (box.width / 2), 1000 - (box.height / 2));
+    //square 3
+    image(square, 900 - (square.width / 2), 1000 - (square.height / 2));
     if (((x > 550) && (x <= 600)) && (y >= 875)) {
       image(alien, 0, 0, c, d);
       if ((c <= 1000) && (d <= 1000)) c += 1.5, d += 1.5;
     } //end if
 
-    //box 4 (original path)
-    image(box, 145 - (box.width / 2), 1000 - (box.height / 2));
+    //square 4 (original path)
+    image(square, 145 - (square.width / 2), 1000 - (square.height / 2));
     if (((x > 700) && (x <= 800)) && (y >= 875)) {
       image(alien, a - (alien.width / 2), b - (alien.height / 2), 100, 100);
       a = random(width)
       b = random(height)
     } //end if
 
-    //box 5 (Target 2)
-    image(box, 745 - (box.width / 2), 1000 - (box.height / 2));
+    //square 5 (Target 2)
+    image(square, 745 - (square.width / 2), 1000 - (square.height / 2));
     if ((x > 800) && (y >= 875)) {
       for (var e = -25; e <= width - 25; e += 250) {
         for (var f = -25; f <= height - 25; f += 250) {
@@ -56,4 +56,4 @@ function Boxes() {
       } //end for f
     } //end if
   }//end show
-} //end boxes
+} //end squarees
