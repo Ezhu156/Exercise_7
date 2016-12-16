65d05376a8b89e05a2e3dd514c84b90227d1ca7c
Ezhu156 <ez682@nyu.edu>
2016-12-15 21:34:34 -0500

    Add files via upload

---
+++ Exercise_7/Targets.js
@@ -0,0 +1,110 @@
+//after working with mousePressed on my project, I now have a better understanding of boolean expressions and realized I could have 
+//done this using boolean expressions so that it would have been more of the way I originally planned
+function Targets() {
+  this.hit = function() {
+      if (value === 0) {
+        //target 1
+        image(target, 880 - (target.width / 2), 205 - (target.height / 2));
+        //target 2
+        image(target, 650 - (target.width / 2), 350 - (target.height / 2));
+        //text("2", 635, 310);
+        //target 3
+        image(target, 175 - (target.width / 2), 350 - (target.height / 2));
+        //text("3", 160, 310);
+        //target 4
+        image(target, 880 - (target.width / 2), 485 - (target.height / 2));
+        //text("4", 920, 500);
+        //target 5
+        image(target, 50 - (target.width / 2), 585 - (target.height / 2));
+        //text("5", 90, 583);
+        //target6
+        image(target, 468 - (target.width / 2), 720 - (target.height / 2));
+        //text("6", 455, 685);
+
+      } //end 0
+      if (value == 1) {
+        //target 1
+        image(alien, 880 - (alien.width / 4), 205 - (alien.height / 4), 100, 100);
+        //target 2
+        image(target, 650 - (target.width / 2), 350 - (target.height / 2));
+        //target 3
+        image(target, 175 - (target.width / 2), 350 - (target.height / 2));
+        //target 4
+        image(target, 880 - (target.width / 2), 485 - (target.height / 2));
+        //target 5
+        image(target, 50 - (target.width / 2), 585 - (target.height / 2));
+        //target6
+        image(target, 468 - (target.width / 2), 720 - (target.height / 2));
+      } //end 1
+      if (value == 2) {
+        //target 2
+        image(alien, 640 - (alien.width / 4), 340 - (alien.height / 4), 100, 100);
+        //target 1
+        image(target, 880 - (target.width / 2), 205 - (target.height / 2));
+        //target 3
+        image(target, 175 - (target.width / 2), 350 - (target.height / 2));
+        //target 4
+        image(target, 880 - (target.width / 2), 485 - (target.height / 2));
+        //target 5
+        image(target, 50 - (target.width / 2), 585 - (target.height / 2));
+        //target6
+        image(target, 468 - (target.width / 2), 720 - (target.height / 2));
+      } //end 2
+      if (value == 3) {
+        //target 3
+        image(alien, 170 - (alien.width / 4), 330 - (alien.height / 4), 100, 100);
+        //target 1
+        image(target, 880 - (target.width / 2), 205 - (target.height / 2));
+        //target 2
+        image(target, 650 - (target.width / 2), 350 - (target.height / 2));
+        //target 4
+        image(target, 880 - (target.width / 2), 485 - (target.height / 2));
+        //target 5
+        image(target, 50 - (target.width / 2), 585 - (target.height / 2));
+        //target6
+        image(target, 468 - (target.width / 2), 720 - (target.height / 2));
+      } //end 3
+      if (value == 4) {
+        //target 4
+        image(alien, 890 - (alien.width / 4), 470 - (alien.height / 4), 100, 100);
+        //target 1
+        image(target, 880 - (target.width / 2), 205 - (target.height / 2));
+        //target 2
+        image(target, 650 - (target.width / 2), 350 - (target.height / 2));
+        //target 3
+        image(target, 175 - (target.width / 2), 350 - (target.height / 2));
+        //target 5
+        image(target, 50 - (target.width / 2), 585 - (target.height / 2));
+        //target6
+        image(target, 468 - (target.width / 2), 720 - (target.height / 2));
+      } //end 4
+      if (value == 5) {
+        //target 5
+        image(alien, 50 - (alien.width / 4), 570 - (alien.height / 4), 100, 100);
+        //target 1
+        image(target, 880 - (target.width / 2), 205 - (target.height / 2));
+        //target 2
+        image(target, 650 - (target.width / 2), 350 - (target.height / 2));
+        //target 3
+        image(target, 175 - (target.width / 2), 350 - (target.height / 2));
+        //target 4
+        image(target, 880 - (target.width / 2), 485 - (target.height / 2));
+        //target6
+        image(target, 468 - (target.width / 2), 720 - (target.height / 2));
+      } //end 5
+      if (value == 6) {
+        //target6
+        image(alien, 460 - (alien.width / 4), 700 - (alien.height / 4), 100, 100);
+        //target 1
+        image(target, 880 - (target.width / 2), 205 - (target.height / 2));
+        //target 2
+        image(target, 650 - (target.width / 2), 350 - (target.height / 2));
+        //target 3
+        image(target, 175 - (target.width / 2), 350 - (target.height / 2));
+        //target 4
+        image(target, 880 - (target.width / 2), 485 - (target.height / 2));
+        //target 5
+        image(target, 50 - (target.width / 2), 585 - (target.height / 2));
+      } //end 6
+    } //end hit
+} //end target
